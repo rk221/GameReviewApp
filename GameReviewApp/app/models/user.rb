c4f4d85206db9d9d8d9d18e38a9bf6b464549ba1
rk <kd1242488@st.kobedenshi.ac.jp>
2020-02-04 13:11:15 +0900

    Add UserView New

--- GameReviewApp/app/models/user.rb
+++ GameReviewApp/app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
     has_secure_password
     has_many :reviews
     has_many :likes_for_user_reviews
+
+    validates :name, presence: true
+    #email validates
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+    
 end
