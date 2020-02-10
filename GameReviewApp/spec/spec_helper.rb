986d36fc761741d7b585d352627a94ed6662f267
rk <kd1242488@st.kobedenshi.ac.jp>
2020-02-10 21:23:19 +0900

    Add Spec Config

--- GameReviewApp/spec/spec_helper.rb
+++ GameReviewApp/spec/spec_helper.rb
@@ -13,7 +13,15 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+#RSpec Test Use Capybara
+require 'capybara/rspec'
+
 RSpec.configure do |config|
+  #RSpec test browser chrome_headless
+  config.before(:each, type: :system) do
+    driven_by :selenium_chrome_headless
+  end
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
