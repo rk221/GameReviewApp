919a20f6c497d1910fc2692aca14a882bd5c84d9
rk <kd1242488@st.kobedenshi.ac.jp>
2020-02-15 17:38:58 +0900

    Add UserModel Validation
    ユーザーモデルに長さチェックのバリエーション追加

--- GameReviewApp/app/models/user.rb
+++ GameReviewApp/app/models/user.rb
@@ -3,9 +3,10 @@ class User < ApplicationRecord
     has_many :reviews
     has_many :likes_for_user_reviews
 
-    validates :name, presence: true
+    validates :name, presence: true, length: {maximum: 30}
+
     #email validates
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
-    
+    validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }, length: {maximum: 254}
+    validates :password, presence: true, length: {minimum: 8, maximum:128}
 end
