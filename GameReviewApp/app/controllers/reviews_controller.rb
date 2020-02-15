ef4e83132ca062562b3079ed24e4e3205acd1b54
rk <kd1242488@st.kobedenshi.ac.jp>
2020-02-15 21:42:26 +0900

    Add ReviewsController And View
    r[e@\Aꗗ\¦@\ð¼ǉÁB
    »êɔº¢A[eBOÆSeedsðύX
    ¼³µ̃r[ªeł«Ăµ܂¤Br[̃^Cgª¶݂µȂ¢oO èB

---
+++ GameReviewApp/app/controllers/reviews_controller.rb
@@ -0,0 +1,27 @@
+class ReviewsController < ApplicationController
+    def index
+        @reviews = Review.all
+    end
+
+    def new
+        @review = Review.new
+    end
+
+    def create
+        @review = Review.new(review_params)
+        @review.user_id = current_user.id
+        if @review.save
+            redirect_to reviews_path, notice: 'レビューを投稿しました'
+        else
+            render :new
+        end
+    end
+    
+    private
+
+    def review_params
+        params.require(:review).permit(:id, :comment, :game_id, :user_id)
+    end
+
+
+end
