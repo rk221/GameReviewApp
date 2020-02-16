81604774d65da5289237074093b5d2da5e42dde5
rk <kd1242488@st.kobedenshi.ac.jp>
2020-02-16 15:11:05 +0900

    Add ReviewModel Validation
    レビューモデルにバリデーション追加

--- GameReviewApp/app/models/review.rb
+++ GameReviewApp/app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ApplicationRecord
     belongs_to :user
     belongs_to :game
+
+    validates :title, presence: true, length: {maximum: 50}
+    validates :comment, presence: true, length: {maximum: 200}
+
 end
