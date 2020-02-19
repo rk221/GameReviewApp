85c1a3e773a2e2d2a0150ca84fac2029accfe635
rk <kd1242488@st.kobedenshi.ac.jp>
2020-02-19 15:15:05 +0900

    Add LikesForUsersController

---
+++ GameReviewApp/app/controllers/likes_for_users_controller.rb
@@ -0,0 +1,5 @@
+class LikesForUsersController < ApplicationController
+    def index
+
+    end
+end
---
+++ GameReviewApp/test/controllers/likes_for_users_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class LikesForUsersControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
