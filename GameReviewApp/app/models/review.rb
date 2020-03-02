892213a409afd5f595d88d7b77298bef89de7568
rk <kd1242488@st.kobedenshi.ac.jp>
2020-03-02 16:43:43 +0900

    Add Model Validate to Review Rate

--- GameReviewApp/app/models/review.rb
+++ GameReviewApp/app/models/review.rb
@@ -5,6 +5,7 @@ class Review < ApplicationRecord
 
     validates :title, presence: true, length: {maximum: 50}
     validates :comment, presence: true, length: {maximum: 200}
+    validates :rate, presence: true, numericality: {less_than_or_equal_to: 5, greater_than_or_equal_to: 1}
 
     mount_uploader :image, ImageUploader
 end
