39cfe127ec835a1cf2123b301a507c16d6f322f6
rk <kd1242488@st.kobedenshi.ac.jp>
2020-03-25 20:23:35 +0900

    Change UserEdit JS
    [U[ҏWÌJavaScriptà̏璷ȃR[hð¤ʉ»

--- GameReviewApp/app/assets/javascripts/users.js
+++ GameReviewApp/app/assets/javascripts/users.js
@@ -1,9 +1,5 @@
 $(document).on('turbolinks:load',function() {
-    $('#name_menu').on('click',function(){
-        $('#name_form').toggle();
-    });
-
-    $('#password_menu').on('click',function(){
-        $('#password_form').toggle();
+    $('.toggle_form').on('click',function(){
+        $(this).children(".toggle_target").toggle();
     });
 });
